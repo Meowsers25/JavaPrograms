2e62b2c082fbf65887b17a68249c008d2035c8d2
Chris <c.lamothe25@gmail.com>
2017-10-28 12:40:10 -0400

    added CharAndBool

---
+++ CharAndBool/src/com/chrislamothe/Main.java
@@ -0,0 +1,15 @@
+package com.chrislamothe;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char myChar = '\u07F7';
+        System.out.println("Really cool unicode output is " + myChar);
+
+        boolean myBoolean = true;
+
+        char newChar = '\u00AE';
+        System.out.println("Registered symbol is " + newChar);
+
+    }
+}
